29935f5968d47a8756e8d3f2db16b8d3c1e89629
ankush91 <ankushisraney@gmail.com>
2019-12-12 15:20:59 -0500

    adding soln for part-2 of day-12

--- src/day_12/day_12.py
+++ src/day_12/day_12.py
@@ -1,4 +1,5 @@
 import copy
+from math import gcd
 
 
 def read_input():
@@ -67,27 +68,48 @@ def main():
     all_moon_coordinates_initial, all_moon_velocities_initial = read_input()
     all_moon_coordinates = copy.deepcopy(all_moon_coordinates_initial)
     all_moon_velocities = copy.deepcopy(all_moon_velocities_initial)
-    i = 0
-    ## change to 1000 for PART-1; else repeat till equality achieved
-    while True:
-        i += 1
-        all_moon_velocities = apply_gravity(all_moon_coordinates, all_moon_velocities,)
-        all_moon_coordinates = apply_velocity(all_moon_coordinates, all_moon_velocities,)
+    ## change to 1000 for PART-1 and remove outer-loop with break cases;
+    ##  else repeat till equality of independent axis achieved achieved
 
-        if ((all_moon_coordinates == all_moon_coordinates_initial) and 
-            (all_moon_velocities == all_moon_velocities_initial)):
-            print(all_moon_coordinates_initial)
-            break
+    simulate_axis_cycles = []
+    # try to achieve repetition between independent axes
+    for j in range(len(all_moon_coordinates[0])):
+        i = 0
+        set_sum_cols = set()
+        prev_len_sum_cols = 0
+        while True:
+            all_moon_coordinates_tup = tuple([a[j] for a in all_moon_coordinates],)
+            all_moon_velocities_tup = tuple([a[j] for a in all_moon_velocities],)
+            set_sum_cols.add((all_moon_coordinates_tup, all_moon_velocities_tup))
+            if len(set_sum_cols) == prev_len_sum_cols:
+                break
+            
+            i += 1
+            all_moon_velocities = apply_gravity(all_moon_coordinates, all_moon_velocities,)
+            all_moon_coordinates = apply_velocity(all_moon_coordinates, all_moon_velocities,)
 
-    potential_energies = [sum(abs(c) for c in m) for m in all_moon_coordinates]
-    kinetic_energies = [sum(abs(c) for c in m) for m in all_moon_velocities]
+            if i % 100000 == 0:
+                print(i)
+            prev_len_sum_cols = len(set_sum_cols)
 
-    print('number of steps', i)
-    print('potential-energies:', potential_energies)
-    print('kinetic-energies:', kinetic_energies)
-    total_energy = sum(potential_energies[i] * kinetic_energies[i] 
-                       for i in range(len(potential_energies)))
-    print('total-energy:', total_energy)
+        simulate_axis_cycles.append(i)
+        
+    # potential_energies = [sum(abs(c) for c in m) for m in all_moon_coordinates]
+    # kinetic_energies = [sum(abs(c) for c in m) for m in all_moon_velocities]
+    # total_energy = sum(potential_energies[i] * kinetic_energies[i] 
+    #                   for i in range(len(potential_energies)))  
+
+    # print('number of steps', i)
+    # print('potential-energies:', potential_energies)
+    # print('kinetic-energies:', kinetic_energies)
+    # print('total-energy:', total_energy)
+    print(simulate_axis_cycles)
+
+    # LCM of independent cycles
+    lcm = simulate_axis_cycles[0]
+    for ax_reps in simulate_axis_cycles[1:]:
+        lcm = int(lcm * ax_reps/gcd(lcm, int(ax_reps)))
+    print('LCM:', lcm)   
 
 
 if __name__ == "__main__":
