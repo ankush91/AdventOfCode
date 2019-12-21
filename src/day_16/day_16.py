afcc8072f36a6a8213137840627a6b62a22d3c53
ankush91 <ankushisraney@gmail.com>
2019-12-21 13:33:18 -0500

    adding in day-16 part 2 specialized case of general problem defn

--- src/day_16/day_16.py
+++ src/day_16/day_16.py
@@ -1,3 +1,7 @@
+import math
+import sys
+
+
 def read_input():
     with open('input', 'r') as file:
         input_str = file.read().strip('\n')
@@ -34,8 +38,19 @@ def part_1(x_input, x_pattern, num_phases=1):
     return output_list
 
 
-def part_2():
-    pass
+def part_2(input_digits, scalar):
+    p = 0
+    while p < 100:
+        fn_input = 0
+        last_loc = len(input_digits) - 1
+        for digit in input_digits[::-1]:
+            fn_input += (digit * scalar)
+            input_digits[last_loc] = abs(fn_input) % 10
+            last_loc -= 1
+        p += 1
+        print(p)
+
+    return input_digits
 
 
 def main():
@@ -45,20 +60,26 @@ def main():
     
     ## PART-1
     output_list = part_1(list_digits, x_pattern, num_phases=100)
-    print('part-1', output_list)
+    print('part-1', output_list[:8])
 
     ## PART-2 ANALYSIS
-    print(len(list_digits))
-    print(len(list_digits) * 10000)
-    print((len(list_digits) * 10000) / 2)
+    # PART-2 currently is implemented using special knowledge of the starting 7 digit input loc
+    # If the intended offset of 7 digits >= len(list_digits) / 2 then due to the triangluar matrix,
+    # fn(input_i) = fn(input_i + 1) + (input_i) * pattern[1]
+    # digit(input_i) = abs(fn(input_i)) % 10
+    # **The generalized implementation is currently pending**
+    
+    part_2_input = list_digits[:] * 10000
+    output_indx = int(''.join([str(xi) for xi in part_2_input[:7]]))
+    mid_loc = math.floor(len(part_2_input) / 2)
 
-    print(part_1(list_digits[:] * 10000, x_pattern, num_phases=100))
-
-    # retrieve index for 1st 7 digits
-    # output_indx = int(''.join([str(xi) for xi in x_input[:7]]))
-    # print(x_input[:7])
-    # print(output_indx)
-    # print(x_input[output_indx:(output_indx + 8)])
+    # solving using specialized soln
+    if output_indx > mid_loc:
+        output_digits = part_2(part_2_input[output_indx:], x_pattern[1])
+        print('part-2', output_digits[:8])
+    else:
+        print('Need general solution; currently not supported')
+        sys.exit(1)
 
 
 if __name__ == "__main__":
